f057fd9645e249d32b6fed807df606091c715b1e
Melody-ing <m9930716@gmail.com>
2024-04-11 13:50:25 +0800

    fix: Refactor button component implementation

--- src/components/Buttons/index.jsx
+++ src/components/Buttons/index.jsx
@@ -4,7 +4,7 @@ import theme from "../globals/theme";
 
 // <Buttons type={1}>主持</Buttons>
 
-const Buttons = ({ children, type = 2 }) => {
+const Buttons = ({ children, type }) => {
   // type 1 :#996484
   // type 2 :#F5F2F0
   // type 3 :#78B159
@@ -12,22 +12,27 @@ const Buttons = ({ children, type = 2 }) => {
   let bg = "";
   let color = "";
   let hoverbg = "";
-  if (type === 1) {
-    bg = `${theme.colors.secondary}`;
-    color = `${theme.colors.light}`;
-    hoverbg = `${theme.colors.tertiary}`;
-  } else if (type === 2) {
-    bg = `${theme.colors.light}`;
-    color = `${theme.colors.dark}`;
-    hoverbg = `#a5a4a4`;
-  } else if (type === 3) {
-    bg = `${theme.colors.success}`;
-    color = `${theme.colors.light}`;
-    hoverbg = `${theme.colors.info}`;
-  } else if (type === 4) {
-    bg = `${theme.colors.danger}`;
-    color = `${theme.colors.light}`;
-    hoverbg = `${theme.colors.info}`;
+  switch (type) {
+    case 2:
+      bg = `${theme.colors.light}`;
+      color = `${theme.colors.dark}`;
+      hoverbg = `#a5a4a4`;
+      break;
+    case 3:
+      bg = `${theme.colors.success}`;
+      color = `${theme.colors.light}`;
+      hoverbg = `${theme.colors.info}`;
+      break;
+    case 4:
+      bg = `${theme.colors.danger}`;
+      color = `${theme.colors.light}`;
+      hoverbg = `${theme.colors.info}`;
+      break;
+    default:
+      bg = `${theme.colors.secondary}`;
+      color = `${theme.colors.light}`;
+      hoverbg = `${theme.colors.tertiary}`;
+      break;
   }
 
   return (
