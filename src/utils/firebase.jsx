9d8cc8a87b1e6e2457d8471b4fa8891b80865ae8
Melody-ing <m9930716@gmail.com>
2024-05-17 16:29:51 +0800

    set firestore .env

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -10,14 +10,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB8yiMuX2g8mMWuKI1H4NaHOof5pO5YfYM",
-  authDomain: "loody-ing.firebaseapp.com",
-  databaseURL: "https://loody-ing-default-rtdb.firebaseio.com",
-  projectId: "loody-ing",
-  storageBucket: "loody-ing.appspot.com",
-  messagingSenderId: "862682833648",
-  appId: "1:862682833648:web:ac397e7c95af8640555835",
-  measurementId: "G-41LSZ5MVZW",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
